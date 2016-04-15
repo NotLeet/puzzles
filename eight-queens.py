9a0a7154b9ecc3e114f86c12c1bf9f82b964f18b
Sean Matthews <matthews.sean.d@gmail.com>
2016-04-15 07:25:53 -0500

    PEP 8 Fixes

--- eight-queens.py
+++ eight-queens.py
@@ -1,6 +1,3 @@
-import random
-
-
 class Square:
     def __init__(self, column, row):
         self.col_num = column
@@ -15,7 +12,11 @@ class Square:
         return '{}{}'.format(self.col_str, self.row_str)
 
 
-def safe_squares(start_square, all_squares):
+def slope(y2, y1, x2, x1):
+    return (y2 - y1) / (x2 - x1)
+
+
+def safe_squares(queen_pos, all_squares):
     """
     Returns a list of cells that a queen can't
     attack when standing on the input cell.
@@ -24,37 +25,49 @@ def safe_squares(start_square, all_squares):
     :return list:
     """
     return [cell for cell in all_squares
-            if cell.row_num != start_square.row_num and  # same row
-            cell.col_num != start_square.col_num and  # same column
-            cell.col_num - cell.row_num != start_square.col_num - start_square.row_num and  # positive slope diagonal
-            cell.col_num + cell.row_num != start_square.col_num + start_square.row_num  # negative slope diagonal
+            if cell.row_num != queen_pos.row_num and  # same row
+            cell.col_num != queen_pos.col_num and  # same column
+            slope(queen_pos.row_num, cell.row_num, queen_pos.col_num, cell.col_num) not in [1, -1]  # Diagonals
             ]
 
 
-def solve(n, queen_positions=None, squares=None):
+def solve(num_queens, rows, columns, queen_positions=None, squares=None, depth=0):
+    """
+    Recursive function
+    Depth first search
+    with backtracking
+    """
     if queen_positions is None:
         queen_positions = []
-        rows = range(1, n + 1)
-        columns = range(1, n + 1)
-        squares = [Square(column, row) for column in columns for row in rows]
-    if len(queen_positions) < n:
+        squares = [Square(column, row) for column in range(1, columns+1) for row in range(1, rows+1)]
+
+    if len(queen_positions) < num_queens:
         if len(squares) == 0:
-            return False
-        result = False
+            return [False, False]
+        result = [False, False]
         dead_squares = []
-        while result is False:
-            try:
-                queen = [square for square in squares if square not in dead_squares][0]
-            except IndexError:
-                return False
-            queen_positions.append(queen)
-            new_squares = safe_squares(queen, squares)
-            result = solve(n, queen_positions=queen_positions, squares=new_squares)
-            if result is False:
-                dead_squares.append(queen)
+        while result[0] is False:
+            safe_squares_minus_dead_squares = [square for square in squares if square not in dead_squares]
+            if len(safe_squares_minus_dead_squares) > 0:
+                new_queen = safe_squares_minus_dead_squares[0]
+            else:
+                return [False, True]
+            queen_positions.append(new_queen)
+            new_squares = safe_squares(new_queen, squares)
+            result = solve(num_queens, rows, columns, queen_positions, new_squares, depth+1)
+            if result[0] is False:
+                dead_squares.append(new_queen)
+                queen_positions.pop()
         return result
     else:
         return queen_positions
 
 
-print(solve(8))
+def main(n):
+    result = solve(n, n, n)
+    if result[0] is False:
+        print('2x2 and 3x3 boards do not have solutions')
+    else:
+        print(result)
+
+main(8)
